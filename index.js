c5d6e121836d135ab44aa38dad7ec21e5c4ea5ac
beji <simone0443@outlook.com>
2024-04-04 15:47:37 +0200

    add data persistence

--- index.js
+++ index.js
@@ -1,28 +1,54 @@
-var inputText, todo, body;
+var inputText, todo, body; 
+var todos = [];
+var storedTodos;
 window.onload = (event) => {
     // console.log("page is fully loaded");
-    inputText = document.getElementById("inputText");
+
     body = document.getElementById("body");
-};
-// toastr
-function addTodo()
-{
-    todo = inputText.value;
-    
-    if(todo.length == 0) {
-        console.log("ahh");
-        return;
+    storedTodos = JSON.parse(localStorage.getItem("todos"));
+    if(storedTodos.length != 0)
+    {
+        for(var i = 0; i < storedTodos.length; i++)
+        {
+            todos.push(storedTodos[i]);
+            addDiv(storedTodos[i]);
+        }
     }
 
+    // console.log(storedTodos);
+    console.log(todos);
+    inputText = document.getElementById("inputText");
+    inputText.setAttribute('autocomplete', 'off');
+};
+
+function addDiv(todoText)
+{
     const todosDiv = document.createElement("div");
-    todosDiv.classList.add(...['m-4','bg-zinc-800', 'p-6', 'text-gre-600', 'rounded-xl', 'max-w-sm', 'flex', 'items-center', 'mx-auto']);
+    todosDiv.classList.add(...['m-4','bg-zinc-800', 'px-2', 'py-1', 'text-gray-600', 'rounded-xl', 'max-w-sm', 'flex', 'items-center', 'mx-auto']);
 
     //<label class="text-xl font-medium text-white">Todo</label>
 
     const text = document.createElement("label");
-    text.innerHTML = todo;
+    text.innerHTML = todoText;
+    text.classList.add(...['text-white']);
 
-    text.classList.add(...['text-xl', 'font-medium', 'text-white']);
     todosDiv.appendChild(text);
     body.appendChild(todosDiv);
+
+}
+
+function addTodo()
+{
+    todo = inputText.value;
+    if(todo.length == 0) {
+        //toastr
+        return;
+    }
+
+    todos.push(todo);
+
+    console.log(todos);
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+    addDiv(todo);
 }
